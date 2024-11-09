39f7046e89a2f14fa19f1843131e5eb0f90f8fe6
Wonseok <dnjstjr970302@gmail.com>
2024-11-09 22:41:35 +0900

    이미지 로더 및 클라우디너리 크기 조정 사용

--- components/posts.js
+++ components/posts.js
@@ -7,11 +7,24 @@ import LikeButton from './like-icon';
 import { togglePostLikeStatus } from '@/actions/posts';
 import Image from 'next/image';
 
+function imageLoader(config) {
+  const urlStart = config.src.split('upload/')[0];
+  const urlEnd = config.src.split('upload/')[1];
+  const transformations = `w_200,h_150,q_${config.quality}`;
+  return `${urlStart}upload/${transformations}/${urlEnd}`;
+}
+
 function Post({ post, action }) {
   return (
     <article className='post'>
       <div className='post-image'>
-        <Image src={post.image} fill alt={post.title} />
+        <Image
+          src={post.image}
+          loader={imageLoader}
+          width={200}
+          height={120}
+          alt={post.title}
+        />
       </div>
       <div className='post-content'>
         <header>
